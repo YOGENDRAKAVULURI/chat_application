5e2b3bf18b6c4787162dc3b4e89b79816145dab4
YOGENDRAKAVULURI <yogendrakavuluri8@gmail.com>
2025-12-12 13:55:18 +0530

    Update cmail.py

--- cmail.py
+++ cmail.py
@@ -1,26 +1,25 @@
-from mailersend import emails
 import os
+from mailersend import MailerSend
 
 def send_email(to, subject, body):
     try:
-        mailer = emails.NewEmail(os.environ["MAILERSEND_API_KEY"])
+        mailer = MailerSend(api_key=os.environ["MAILERSEND_API_KEY"])
 
-        mail_body = {
-            "from": {
-                "email": "noreply@mailersend.net",
-                "name": "Chat App"
-            },
-            "to": [
-                {
-                    "email": to
-                }
-            ],
-            "subject": subject,
-            "text": body
-        }
+        mailer.email.send(
+            {
+                "from": {
+                    "email": "noreply@mailersend.net",
+                    "name": "Chat App"
+                },
+                "to": [
+                    {"email": to}
+                ],
+                "subject": subject,
+                "text": body
+            }
+        )
 
-        mailer.send(mail_body)
-        print("Mailersend success")
+        print("Mail sent successfully")
         return True
 
     except Exception as e:
