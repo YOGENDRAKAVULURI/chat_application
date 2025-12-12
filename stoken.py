dbda3c341a7e7f527937a3a09d3a3b62f88e33e9
YOGENDRAKAVULURI <yogendrakavuluri8@gmail.com>
2025-12-12 12:18:58 +0530

    Refactor token generation and verification functions

--- stoken.py
+++ stoken.py
@@ -1,11 +1,12 @@
 from itsdangerous import URLSafeTimedSerializer
-secret_key='Daimondsai'
-salt='saicharan'
 
-def generate_token(data):
-    serializer = URLSafeTimedSerializer(secret_key)
-    return serializer.dumps(data, salt=salt)
+secret_key = "Daimondsai"
+salt = "saicharan"
 
-def verify_token(data):
+def generate_token(payload):
     serializer = URLSafeTimedSerializer(secret_key)
-    return serializer.loads(data, salt=salt)
+    return serializer.dumps(payload, salt=salt)
+
+def verify_token(token):
+    serializer = URLSafeTimedSerializer(secret_key)
+    return serializer.loads(token, salt=salt)
